02ba838b53111a81f2648dadbfd8025acd0c23b5
ChitrakshG <chitrakshgupta@jklu.edu.in>
2024-02-14 19:59:10 +0530

    sms-spam

---
+++ sms-spam-classifier/app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import string
+
+from nltk.corpus import stopwords
+import nltk
+import string
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+
+tfidf=pickle.load(open("vectorizer.pkl",'rb'))
+model=pickle.load(open('model.pkl','rb'))
+
+st.title("Email/Sms Spam Classifier")
+
+input_sms=st.text_area("enter the message")
+
+
+def transform_text(text):
+    text = text.lower()  # lowercase
+    text = nltk.word_tokenize(text)  # tokenization :- convert sentence in words list
+    # removing special char
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    # removing stop words in puncutaion
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))  # remove s , ing
+    text = y[:]
+    y.clear()
+    return " ".join(text)
+if st.button("Predict"):
+    #1. preprocess
+    transformed_sms=transform_text(input_sms)
+    #2. vectorize
+    vector_input=tfidf.transform([transformed_sms])
+    #3.predict
+    result=model.predict(vector_input)[0]
+    #4.display
+    if result==1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
